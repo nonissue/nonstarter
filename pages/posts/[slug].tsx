13b6594c4030c7a3fea61feee0196b0da57e2807
nonissue <andy@nonissue.org>
2020-10-18 01:31:39 -0600

    vercel env

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -44,7 +44,7 @@ const PostPage: React.FunctionComponent<{ post: string }> = ({ post }) => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [{ params: { slug: "1234" } }],
+    paths: [{ params: { slug: "1" } }],
     fallback: false
   };
 };
